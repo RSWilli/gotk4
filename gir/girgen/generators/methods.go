92c68ac935146920a5a69086a2ff04930773233f
wbartel <wilhelm.bartel@streamonkey.de>
2025-05-02 11:32:26 +0200

    document MethodGenerator interface

--- gir/girgen/generators/methods.go
+++ gir/girgen/generators/methods.go
@@ -7,6 +7,9 @@ type MethodGeneratorList []MethodGenerator
 type MethodGenerator interface {
 	Generator
 
+	// GenerateInterfaceSignature generates the interface signature for the method.
+	// this is needed because the snytax differs from the normal method signatures and
+	// the instance parameter is implicit.
 	GenerateInterfaceSignature(w file.File)
 }
 
@@ -23,7 +26,7 @@ func (list MethodGeneratorList) Generate(w *file.Package) {
 	}
 }
 
-// GenerateInterfaceSignature implements MethodGenerator.
+// GenerateInterfaceSignatures iterates over the list of MethodGenerators and calls GenerateInterfaceSignature on each one.
 func (list MethodGeneratorList) GenerateInterfaceSignatures(w file.File) {
 	for _, g := range list {
 		if g == nil {
@@ -31,5 +34,35 @@ func (list MethodGeneratorList) GenerateInterfaceSignatures(w file.File) {
 		}
 
 		g.GenerateInterfaceSignature(w)
+	}
+}
+
+type VirtualMethodGeneratorList []*VirtualMethodGenerator
+
+// overridesInstanceGenericType is the generic type in the overrides struct that will be used as
+// the instance type.
+var overridesInstanceGenericType = "Instance"
+
+var _ Generator = (VirtualMethodGeneratorList)(nil)
+
+// Generate implements Generator.
+func (list VirtualMethodGeneratorList) Generate(w *file.Package) {
+	for _, g := range list {
+		if g == nil {
+			continue
+		}
+
+		g.Generate(w)
+	}
+}
+
+// GenerateClassOverrideFields iterates over the list of VirtualMethodGenerators and calls GenerateClassOverrideField on each one.
+func (list VirtualMethodGeneratorList) GenerateClassOverrideFields(w file.File) {
+	for _, g := range list {
+		if g == nil {
+			continue
+		}
+
+		g.GenerateClassOverrideField(w)
 	}
 }
