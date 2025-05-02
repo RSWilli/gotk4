c922409941a2f78524c6f137283f8b8fb1ef2a87
RSWilli <bartel.wilhelm@gmail.com>
2025-05-02 15:10:28 +0200

    remove gobject introspection pkg-config dependency fo gerror

--- pkg/glib/v2/gerror.go
+++ pkg/glib/v2/gerror.go
@@ -1,11 +1,8 @@
 package glib
 
-// #cgo pkg-config: glib-2.0 gobject-introspection-1.0
+// #cgo pkg-config: glib-2.0
 // #cgo CFLAGS: -Wno-deprecated-declarations
-// #include <glib-object.h>
-// #include <gmodule.h>
 // #include <glib.h>
-// #include <stdlib.h>
 import "C"
 
 import (
